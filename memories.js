bc378e12a9776903a12d4e0a8f8d37ad9494eb8f
helenpamela <hpgilmore@msn.com>
2026-04-04 12:14:42 -0700

    Add files via upload

---
+++ memories.js
@@ -0,0 +1,95 @@
+
+
+const memories = [
+  {
+    src: "member-images/2020-bookmarks.jpg",
+    caption: "Bookmarks 2020 🔖"
+  },
+  {
+    src: "member-images/2020-christmas-kathy-linda.jpg",
+    caption: "Christmas 2020 - Kathy & Linda🎄"
+  },  
+  {
+    src: "member-images/2020-christmas-pam.jpg",
+    caption: "Christmas 2020 - Pam 🎄"
+  },
+  {
+    src: "member-images/2020-christmas-pc-trudi.jpg",
+    caption: "Christmas 2020 - PC & Trudi 🎄"
+  },
+  {
+    src: "member-images/2020-dalai-linda-birthday.jpg",
+    caption: "Dalai Linda - Birthday 2020 🎂"
+  },
+  {
+    src: "member-images/2020-five-of-us-in-living-room.jpg",
+    caption: "Living Room - 2020 🌸"
+  },
+  {
+    src: "member-images/2020-kathy-cheers!.jpg",
+    caption: "Cheers! - Kathy 2020 🍷"  
+  },
+  {
+    src: "member-images/2020-kathy-rbg.jpg",
+    caption: "Kathy 2020 - RBG Forever 🌟"   
+  },
+  {
+    src: "member-images/2020-linda-birthday-sparkly-mask.jpg",
+    caption: "Linda's sparkly birthday mask 2020 🎂"   
+  },
+  {
+    src: "member-images/2020-linda-kenny-flowers.jpg",
+    caption: "Linda & Kenny & Flowers - 2020 🌻🙃"   
+  },
+  {
+    src: "member-images/2020-linda-with-doll.jpg",
+    caption: "Is that a voodoo doll? 🙃"     
+  },
+  {
+    src: "member-images/2020-mellow-mushroom.jpg",
+    caption: "Being mellow at the Mellow Mushroom 2020 🍕"     
+  },
+  {
+    src: "member-images/2020-pam-linda-burning-bowl-ceremony.jpg",
+    caption: "Pam & Linda - Burning Bowl Ceremony 2020 🔥"      
+  },
+  {
+    src: "member-images/2020-pam-pc-birthday-may.jpg",
+    caption: "Pam & May birthday girl PC 🎁"      
+  },
+  {
+    src: "member-images/2020-pams-watercolors.jpg",
+    caption: "Pam's watercolors 2020 🎨"      
+  },
+  {
+    src: "member-images/2020-pc-trudi-brrrr.jpg",
+    caption: "PC & Trudi 2020 - Brrrrr ❄️"      
+  },
+  {
+    src: "member-images/2020-pc-trudi-pam-april-birthdays.jpg",
+    caption: "PC & April birthday girls 🎈"      
+  },
+  {
+    src: "member-images/2020-trudi-up!.jpg",
+    caption: "2020 - Up! 📿"      
+  },
+  {
+    src: "member-images/2020-vision-boards.jpg",
+    caption: "2020 Vision Boards 🖼️"      
+  },
+  {
+    src: "member-images/2020-wearing-masks-and-with-poops.jpg",
+    caption: "Masks & Poops - 2020 💩"      
+  },
+  {
+    src: "member-images/2020-year-of-the-cute-rat.jpg",
+    caption: "2020 Year of the Cute Rat 🐭"      
+  },
+  {
+    src: "member-images/2021-dolphins.jpg",
+    caption: "Dolphins 2021 🐬"      
+  }
+
+
+
+];
