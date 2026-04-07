4cc4ad56c5ca5dba8902e78bfb6249103d96aa89
helenpamela <hpgilmore@msn.com>
2026-04-07 11:24:58 -0700

    Add files via upload

--- memories.js
+++ memories.js
@@ -4,59 +4,73 @@ const memories = {
   early: [
     {
       src: "member-images/pam-kiki-cropped.jpg",
-      caption: "Pam & Kiki 2014 🐶"
+      caption: "Pam & Kiki 2014 🐶",
+      year: 2014
     },
     {
       src: "member-images/linda_cropped.jpg",
-      caption: "Linda 2014 😃"
+      caption: "Linda 2014 😃",
+      year: 2014
     },
     {
       src: "member-images/pam_cropped.jpg",
-      caption: "Pam 2014 😃"
+      caption: "Pam 2014 😃",
+      year: 2014
     },
     {
       src: "member-images/pc_cropped.jpg",
-      caption: "PC 2014 😃"
+      caption: "PC 2014 😃",
+      year: 2014
     },
     {
       src: "member-images/trudy-selma-Cropped-2014.jpg",
-      caption: "Trudi & Selma 2014 🐶"
+      caption: "Trudi & Selma 2014 🐶",
+      year: 2014
     },
     {
       src: "member-images/vision-boards-2013.jpg",
-      caption: "Vision Boards 2013 🖼️"
+      caption: "Vision Boards 2013 🖼️",
+      year: 2013
     },
     {
       src: "member-images/vision-boards-2014.jpg",
-      caption: "Vision Boards 2014 🖼️"
+      caption: "Vision Boards 2014 🖼️",
+      year: 2014
     },
     {
       src: "member-images/our-favorite-photo-2010.jpg",
-      caption: "Our Favorite Photo 2010 💖"
+      caption: "Our Favorite Photo 2010 💖",
+      year: 2010
     },
     {
       src: "member-images/scarves-2013.jpg",
-      caption: "Scarves 2013 🧣"
+      caption: "Scarves 2013 🧣",
+      year: 2013
     },
     {
       src: "member-images/st-pat-kerchiefs-cropped-2013.jpg",
-      caption: "St. Patrick's Day 2013 ☘️"
+      caption: "St. Patrick's Day 2013 ☘️",
+      year: 2013
     },
     {
       src: "member-images/sunflowers-2012.jpg",
-      caption: "Sunflowers 2012 ☘️"
+      caption: "Sunflowers 2012 ☘️",
+      year: 2012
     },
     {
       src: "member-images/crazy-glasses-2012.jpg",
-      caption: "Silly Glasses 2012 👓"
+      caption: "Silly Glasses 2012 👓",
+      year: 2012
     },
     {
       src: "member-images/2014-vision-boards.jpg",
-      caption: "Vision Boards 2014 🖼️"
+      caption: "Vision Boards 2014 🖼️",
+      year: 2014
     },
     {
-      src: "member-images/kathy_cropped.jpg",
-      caption: "Kathy 2014 😃"
+      src: "member-images/kathy-cropped.jpg",
+      caption: "Kathy 2014 😃",
+      year: 2014
     }
 
 
@@ -66,107 +80,133 @@ const memories = {
   middle: [
     {
       src: "member-images/2020-bookmarks.jpg",
-      caption: "Bookmarks 2020 🔖"
+      caption: "Bookmarks 2020 🔖",
+      year: 2020
     },
     {
       src: "member-images/2020-christmas-kathy-linda.jpg",
-      caption: "Christmas 2020 - Kathy & Linda🎄"
+      caption: "Christmas 2020 - Kathy & Linda🎄",
+      year: 2020
     },
     {
       src: "member-images/2020-christmas-pam.jpg",
-      caption: "Christmas 2020 - Pam 🎄"
+      caption: "Christmas 2020 - Pam 🎄",
+      year: 2020
     },
     {
       src: "member-images/2020-christmas-pc-trudi.jpg",
-      caption: "Christmas 2020 - PC & Trudi 🎄"
+      caption: "Christmas 2020 - PC & Trudi 🎄",
+      year: 2020
     },
     {
       src: "member-images/2020-dalai-linda-birthday.jpg",
-      caption: "Dalai Linda - Birthday 2020 🎂"
+      caption: "Dalai Linda - Birthday 2020 🎂",
+      year: 2020
     },
     {
       src: "member-images/2020-five-of-us-in-living-room.jpg",
-      caption: "Living Room - 2020 🌸"
+      caption: "Living Room - 2020 🌸",
+      year: 2020
     },
     {
       src: "member-images/2020-kathy-cheers!.jpg",
-      caption: "Cheers! - Kathy 2020 🍷"
+      caption: "Cheers! - Kathy 2020 🍷",
+      year: 2020
     },
     {
       src: "member-images/2020-kathy-rbg.jpg",
-      caption: "Kathy 2020 - RBG Forever 🌟"
+      caption: "Kathy 2020 - RBG Forever 🌟",
+      year: 2020
     },
     {
       src: "member-images/2020-linda-birthday-sparkly-mask.jpg",
-      caption: "Linda's sparkly birthday mask 2020 🎂"
+      caption: "Linda's sparkly birthday mask 2020 🎂",
+      year: 2020
     },
     {
       src: "member-images/2020-linda-kenny-flowers.jpg",
-      caption: "Linda & Kenny & Flowers - 2020 🌻🙃"
+      caption: "Linda & Kenny & Flowers - 2020 🌻🙃",
+      year: 2020
     },
     {
       src: "member-images/2020-linda-with-doll.jpg",
-      caption: "Is that a voodoo doll? 🙃"
+      caption: "Is that a voodoo doll? 🙃",
+      year: 2020
     },
     {
       src: "member-images/2020-mellow-mushroom.jpg",
-      caption: "Being mellow at the Mellow Mushroom 2020 🍕"
+      caption: "Being mellow at the Mellow Mushroom 2020 🍕",
+      year: 2020
     },
     {
       src: "member-images/2020-pam-linda-burning-bowl-ceremony.jpg",
-      caption: "Pam & Linda - Burning Bowl Ceremony 2020 🔥"
+      caption: "Pam & Linda - Burning Bowl Ceremony 2020 🔥",
+      year: 2020
     },
     {
       src: "member-images/2020-pam-pc-birthday-may.jpg",
-      caption: "Pam & May birthday girl PC 🎁"
+      caption: "Pam & May birthday girl PC 🎁",
+      year: 2020
     },
     {
       src: "member-images/2020-pams-watercolors.jpg",
-      caption: "Pam's watercolors 2020 🎨"
+      caption: "Pam's watercolors 2020 🎨",
+      year: 2020
     },
     {
       src: "member-images/2020-pc-trudi-brrrr.jpg",
-      caption: "PC & Trudi 2020 - Brrrrr ❄️"
+      caption: "PC & Trudi 2020 - Brrrrr ❄️",
+      year: 2020
     },
     {
       src: "member-images/2020-pc-trudi-pam-april-birthdays.jpg",
-      caption: "PC & April birthday girls 🎈"
+      caption: "PC & April birthday girls 🎈",
+      year: 2020
     },
     {
       src: "member-images/2020-trudi-up!.jpg",
-      caption: "2020 - Up! 📿"
+      caption: "2020 - Up! 📿",
+      year: 2020
     },
     {
       src: "member-images/2020-vision-boards.jpg",
-      caption: "2020 Vision Boards 🖼️"
+      caption: "2020 Vision Boards 🖼️",
+      year: 2020
     },
     {
       src: "member-images/2020-wearing-masks-and-with-poops.jpg",
-      caption: "Masks & Poops - 2020 💩"
+      caption: "Masks & Poops - 2020 💩",
+      year: 2020
     },
     {
       src: "member-images/2020-year-of-the-cute-rat.jpg",
-      caption: "2020 Year of the Cute Rat 🐭"
+      caption: "2020 Year of the Cute Rat 🐭",
+      year: 2020
     },
     {
       src: "member-images/reindeer-antlers1.jpg",
-      caption: "Reindeer Antlers 2016 🫎"
+      caption: "Reindeer Antlers 2016 🫎",
+      year: 2016
     },
     {
       src: "member-images/elephants.jpg",
-      caption: "elephants 2015 🐘"
+      caption: "elephants 2015 🐘",
+      year: 2015
     },
     {
       src: "member-images/vision-boards-2016.jpg",
-      caption: "Vision Boards 2016 🖼️"
+      caption: "Vision Boards 2016 🖼️",
+      year: 2016
     },
     {
       src: "member-images/cliff-notes-cropped-2019.jpg",
-      caption: "Cliff Notes 2019 📖"
+      caption: "Cliff Notes 2019 📖",
+      year: 2019
     },
     {
       src: "member-images/2020-linda-pc-pam-trudi-village-inn.jpg",
-      caption: "Village Inn 2020 🥪"
+      caption: "Village Inn 2020 🥪",
+      year: 2020
     }
 
 
@@ -176,231 +216,288 @@ const memories = {
   recent: [
     {
       src: "member-images/2021-dolphins.jpg",
-      caption: "Dolphins 2021 🐬"
+      caption: "Dolphins 2021 🐬",
+      year: 2021
     },
     {
       src: "member-images/2021-burning-bowl.jpg",
-      caption: "Burning Bown Ceremony 2021 🔥"
+      caption: "Burning Bown Ceremony 2021 🔥",
+      year: 2021
     },
     {
       src: "member-images/2021-walter-and-linda.jpg",
-      caption: "Walter and Linda 2021 💗"
+      caption: "Walter and Linda 2021 💗",
+      year: 2021
     },
     {
       src: "member-images/2021-linda-and-kenny.jpg",
-      caption: "Linda and Kenny 2021 😎"
+      caption: "Linda and Kenny 2021 😎",
+      year: 2021
     },
     {
       src: "member-images/2021-st-patricks-day.jpg",
-      caption: "St Patrick's Day 2021 ☘️"
+      caption: "St Patrick's Day 2021 ☘️",
+      year: 2021
     },
     {
       src: "member-images/2021-trudi-birthday.jpg",
-      caption: "Trudi's Birthday 2021 🎉"
+      caption: "Trudi's Birthday 2021 🎉",
+      year: 2021
     },
     {
       src: "member-images/2021-pam-birthday.jpg",
-      caption: "Pam's Birthday 2021 🎉"
+      caption: "Pam's Birthday 2021 🎉",
+      year: 2021
     },
     {
       src: "member-images/2021-pc-warm-and-cozy.jpg",
-      caption: "PC Warm & Cozy 2021 ♨️"
+      caption: "PC Warm & Cozy 2021 ♨️",
+      year: 2021
     },
     {
       src: "member-images/2021-pc-how-many-layers.jpg",
-      caption: "PC in May Layers 2021 ♨️"
+      caption: "PC in May Layers 2021 ♨️",
+      year: 2021
     },
     {
       src: "member-images/2021-pc-trudi-pam-bundling-up.jpg",
-      caption: "Bundling Up 2021 ♨️"
+      caption: "Bundling Up 2021 ♨️",
+      year: 2021
     },
     {
       src: "member-images/2021-trudis-thc-field-trip-hi-dharma.jpg",
-      caption: "Trudi's THC Field Trip & Dharma 2021 🚬"
+      caption: "Trudi's THC Field Trip & Dharma 2021 🚬",
+      year: 2021
     },
     {
       src: "member-images/2021-pc-and-flag.jpg",
-      caption: "PC and Flag 2021 🤩"
+      caption: "PC and Flag 2021 🤩",
+      year: 2021
     },
     {
       src: "member-images/2021-princess-kathy.jpg",
-      caption: "Princess Kathleen 2021 👑"
+      caption: "Princess Kathleen 2021 👑",
+      year: 2021
     },
     {
       src: "member-images/2021-trudis-consolation-italian-lunch.jpg",
-      caption: "Trudi's Consolation Italian Lunch 2021 🍴"
+      caption: "Trudi's Consolation Italian Lunch 2021 🍴",
+      year: 2021
     },
     {
       src: "member-images/2021-trudi-apron.jpg",
-      caption: "How do you say Chef in Italian? 2021 🍝"
+      caption: "How do you say Chef in Italian? 2021 🍝",
+      year: 2021
     },
     {
       src: "member-images/2021-trudi-linda-venice.jpg",
-      caption: "Trudi & Linda & Venice 2021 🍴"
+      caption: "Trudi & Linda & Venice 2021 🍴",
+      year: 2021
     },
     {
       src: "member-images/2021-kathy-cozy.jpg",
-      caption: "Kathy Cozy 2021 😺"
+      caption: "Kathy Cozy 2021 😺",
+      year: 2021
     },
     {
       src: "member-images/2021-pam-mask-cozy.jpg",
-      caption: "Pam Cozy 2021 🐶"
+      caption: "Pam Cozy 2021 🐶",
+      year: 2021
     },
     {
       src: "member-images/2021-trudi-cozy.jpg",
-      caption: "Trudi Cozy 2021 😺"
+      caption: "Trudi Cozy 2021 😺",
+      year: 2021
     },
     {
       src: "member-images/2021-pc-cozy.jpg",
-      caption: "PC Cozy 2021 😺"
+      caption: "PC Cozy 2021 😺",
+      year: 2021
     },
     {
       src: "member-images/2021-pendulums-and-kenny.jpg",
-      caption: "Pendulums & Kenny 2021 🔮"
+      caption: "Pendulums & Kenny 2021 🔮",
+      year: 2021
     },
     {
       src: "member-images/2021-christmas-orchids.jpg",
-      caption: "Orhids - Christmas 2021 🌷"
+      caption: "Orhids - Christmas 2021 🌷",
+      year: 2021
     },
     {
       src: "member-images/2021-masks-look-closely.jpg",
-      caption: "Masks 2021 - Look Closely 🫢"
+      caption: "Masks 2021 - Look Closely 🫢",
+      year: 2021
     },
     {
       src: "member-images/2021-christmas-linda.jpg",
-      caption: "Linda - Christmas 2021 🎄"
+      caption: "Linda - Christmas 2021 🎄",
+      year: 2021
     },
     {
       src: "member-images/2021-christmas-linda-pam.jpg",
-      caption: "Linda & Pa - Christmas 2021 🎄"
+      caption: "Linda & Pa - Christmas 2021 🎄",
+      year: 2021
     },
     {
       src: "member-images/2024-vision-boards-pc-trudi-kathy.jpg",
-      caption: "Vision Boards 2024 🖼️"
+      caption: "Vision Boards 2024 🖼️",
+      year: 2024
     },
     {
       src: "2024-year-of-the-dragon-10-dragons.jpg",
-      caption: "Year of 10 Dragons 2024 🖼️"
+      caption: "Year of 10 Dragons 2024 🖼️",
+      year: 2024
     },
     {
       src: "2024-lunch-with-shirts.jpg",
-      caption: "Lunch with Shirts 2024 🖼️"
+      caption: "Lunch with Shirts 2024 🖼️",
+      year: 2024
     },
     {
       src: "member-images/sloths-2025.jpg",
-      caption: "Sloths 2025 🦥"
+      caption: "Sloths 2025 🦥",
+      year: 2025
     },
     {
       src: "member-images/2025-calendars.jpg",
-      caption: "Calendars 2025 📆"
+      caption: "Calendars 2025 📆",
+      year: 2025
     },
     {
       src: "member-images/mimosas.jpg",
-      caption: "Mimosas 🍹"
+      caption: "Mimosas 🍹",
+      year: 2022
     },
     {
       src: "member-images/lunch-with-vicky-2023.jpg",
-      caption: "Lunch with Vicky 2023 🥗"
+      caption: "Lunch with Vicky 2023 🥗",
+      year: 2023
     },
     {
       src: "member-images/group-photo.jpg",
-      caption: "Pink Scarves 2022 🧣"
+      caption: "Pink Scarves 2022 🧣",
+      year: 2022
     },
     {
       src: "member-images/2022-archetype-game.jpg",
-      caption: "Archetype Game 2022 🎴"
+      caption: "Archetype Game 2022 🎴",
+      year: 2022
     },
     {
       src: "member-images/2022-at-randy-rainbow-show.jpg",
-      caption: "Randy Rainbow in Person 2022 🌈"
+      caption: "Randy Rainbow in Person 2022 🌈",
+      year: 2022
     },
     {
       src: "member-images/2022-dalai-linda.jpg",
-      caption: "Dalai Linda 2022 ☯️"
+      caption: "Dalai Linda 2022 ☯️",
+      year: 2022
     },
     {
       src: "member-images/2022-election-vision-board.jpg",
-      caption: "Election Vision Board 2022 🤞🏻"
+      caption: "Election Vision Board 2022 🤞🏻",
+      year: 2022
     },
     {
       src: "member-images/2022-linda-birthday.jpg",
-      caption: "2022 Birthday - What's in the bag? 🎁"
+      caption: "2022 Birthday - What's in the bag? 🎁",
+      year: 2022
     },
     {
       src: "member-images/2022-linda-coloring-book.jpg",
-      caption: "Let Your Light Shine Coloring Book 2022 🖍️"
+      caption: "Let Your Light Shine Coloring Book 2022 🖍️",
+      year: 2022
     },
     {
       src: "member-images/2022-linda-mimosa.jpg",
-      caption: "Cheers! 2022 🍹"
+      caption: "Cheers! 2022 🍹",
+      year: 2022
     },
     {
       src: "member-images/2022-linda-pc-pam-trudi-out-and-about.jpg",
-      caption: "Out and About 2022 🚗"
+      caption: "Out and About 2022 🚗",
+      year: 2022
     },
     {
       src: "member-images/2022-linda-pc-silly-animal-masks.jpg",
-      caption: "2022 Who is that masked stranger? 🦊"
+      caption: "2022 Who is that masked stranger? 🦊",
+      year: 2022
     },
     {
       src: "member-images/2022-lunch-with-the-girls.jpg",
-      caption: "Lunch with the Girls 2022 🍴"
+      caption: "Lunch with the Girls 2022 🍴",
+      year: 2022
     }, 
     {
       src: "member-images/2022-mimosas.jpg",
-      caption: "2022 Mimosas on Easy Street 🍹"
+      caption: "2022 Mimosas on Easy Street 🍹",
+      year: 2022
     },
     {
       src: "member-images/2022-burning-bowl-bucket.jpg",
-      caption: "No, it's not just a bucket 2022 🪣"
+      caption: "No, it's not just a bucket 2022 🪣",
+      year: 2022
     },
     {
       src: "member-images/2022-pam-coloring-book.jpg",
-      caption: "Pam - Coloring Book 2022 🖍️"
+      caption: "Pam - Coloring Book 2022 🖍️",
+      year: 2022
     },
     {
       src: "member-images/2022-pam-linda-archetype-cards.jpg",
-      caption: "Archetype Cards 2022 🎴"
+      caption: "Archetype Cards 2022 🎴",
+      year: 2022
     },
     {
       src: "member-images/2022-pam-trudi-birthday-presents.jpg",
-      caption: "April Birthday Girls 2022 🎉"
+      caption: "April Birthday Girls 2022 🎉",
+      year: 2022
     },
     {
       src: "member-images/2022-pc-smiling.jpg",
-      caption: "PC is happy 2022 🙂"
+      caption: "PC is happy 2022 🙂",
+      year: 2022
     },
     {
       src: "member-images/2022-pc-trudi-archetype cards.jpg",
-      caption: "PC & Trudi - Archetype Cards 2022 🎴"
+      caption: "PC & Trudi - Archetype Cards 2022 🎴",
+      year: 2022
     },
     {
       src: "member-images/2022-randy-rainbow.jpg",
-      caption: "Randy Rainbow Show 2022 🌈"
+      caption: "Randy Rainbow Show 2022 🌈",
+      year: 2022
     },
     {
       src: "member-images/2022-st-patricks-day.jpg",
-      caption: "St. Patrick's Day 2022 ☘️"
+      caption: "St. Patrick's Day 2022 ☘️",
+      year: 2022
     },
     {
       src: "member-images/2022-trudi-i-am-releasing.jpg",
-      caption: "2022 Trudi - I am releasing... 🕉️"
+      caption: "2022 Trudi - I am releasing... 🕉️",
+      year: 2022
     },
     {
       src: "member-images/2022-village-inn.jpg",
-      caption: "Village Inn 2022 🍴"
+      caption: "Village Inn 2022 🍴",
+      year: 2022
     },
     {
       src: "member-images/2022-village-inn-again.jpg",
-      caption: "Village Inn 2022 🍴"
+      caption: "Village Inn 2022 🍴",
+      year: 2022
     },
     {
       src: "member-images/2022-walter-eating-with-sgc.jpg",
-      caption: "Walter joined SGC! 2022 🚀"
+      caption: "Walter joined SGC! 2022 🚀",
+      year: 2022
     },
     {
       src: "member-images/2022-watching-the-burning-bowl.jpg",
-      caption: "Watching it burn 2022 🔥"
+      caption: "Watching it burn 2022 🔥",
+      year: 2022
     }
 
 
